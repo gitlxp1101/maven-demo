807fd1fe30fc5572d3951a71f1c54b80cf151bf2
lxp <1070643216@qq.com>
2020-06-23 22:02:17 +0800

    init

---
+++ maven-persist/src/main/java/com/example/persist/Persist.java
@@ -0,0 +1,11 @@
+package com.example.persist;
+
+public class Persist {
+    public static void main(String[] args) {
+        /**
+         * dependencyManagement下的依赖，子模块需要显示声明，否则不会引入
+         * 这行代码，IDEA就会提示需要引入依赖
+         */
+//        Gson gson = new Gson();
+    }
+}
---
+++ maven-service/src/main/java/com/example/service/GsonTest.java
@@ -0,0 +1,10 @@
+package com.example.service;
+
+import com.google.gson.Gson;
+
+public class GsonTest {
+
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+    }
+}
